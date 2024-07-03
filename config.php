3f49203eb5877b8e72a5dd3a4ab71cb061f502aa
soosbi1 <sebi.g.lesebi@gmail.com>
2024-07-03 13:40:14 +0200

    test

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ $password = '';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-			// set the PDO error mode to exception
-			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// set the PDO error mode to exception
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
